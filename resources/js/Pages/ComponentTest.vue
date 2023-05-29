b6b1289703ee8323b238614d98e297effb552d99
Osamu Uda <uda13@hotmail.co.jp>
2023-05-29 09:34:42 +0900

    inertia vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import GuestLayout from '@/Layouts/GuestLayout.vue'
+    import Lavel from '@/Components/InputLabel.vue'
+    import Input from '@/Components/TextInput.vue'
+
+    const emitTest = e => console.log(e)
+</script>
+
+<template>
+ <GuestLayout>
+    <Lavel value="件名">タイトル</Lavel>
+    <Input 
+        modelValue="初期値" 
+        @update:modelValue="emitTest"
+    ></Input>
+ </GuestLayout>
+
+</template>
