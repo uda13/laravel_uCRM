18d35f962445976dced6d45e5348cec2bdfbe5c2
Osamu Uda <uda13@hotmail.co.jp>
2023-07-08 11:03:10 +0900

    商品関連

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +17,23 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'cut',
+                'memo' => 'cut detail',
+                'price' => 6000,
+            ],
+            [
+                'name' => 'color',
+                'memo' => 'color detail',
+                'price' => 4000,
+            ],
+            [
+                'name' => 'perm( cut included)',
+                'memo' => 'perm detail',
+                'price' => 8000,
+            ],
+            
+        ]);
     }
 }
